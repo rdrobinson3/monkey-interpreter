a9f2ff888f4418955355e429721ad8499e8ee46d
Trey Robinson <rdrobinson3@gmail.com>
2018-03-05 05:25:59 -0500

    Created a basic REPL that prints tokens to stdout

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"github.com/rdrobinson3/monkey/repl"
+)
+
 func main() {
 
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s! this is the Monkey programming language!\n", user.Username)
+	fmt.Printf("Feel free to type in the commands\n")
+	repl.Start(os.Stdin, os.Stdout)
 }
