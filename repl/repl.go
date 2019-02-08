0eda58c2f8d6526844cfd8dfccb4b6888783ff15
Trey Robinson <rdrobinson3@gmail.com>
2019-02-08 05:18:31 -0500

    Updated repl. Update gitignore to exclude workspace.

--- repl/repl.go
+++ repl/repl.go
@@ -6,10 +6,10 @@ import (
 	"io"
 
 	"github.com/rdrobinson3/monkey/lexer"
-	"github.com/rdrobinson3/monkey/token"
+	"github.com/rdrobinson3/monkey/parser"
 )
 
-const PROMPT = ">> "
+const PROMPT = ">>"
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
@@ -23,9 +23,22 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
